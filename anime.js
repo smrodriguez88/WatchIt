beac194165b9528e153116c3ee37952ecce52b00
Emilio Conde <condeemilio891@gmail.com>
2020-09-01 18:46:25 -0500

    anime code working

--- anime.js
+++ anime.js
@@ -1,61 +1,67 @@
-//function to display anime Info 
-// function displayAnimeInfo(anime){
-//   var queryURL="https://kitsu.io/api/edge/anime?filter%5Btext%5D="+anime;
 
-//   //ajax call
-//   $.ajax({
-//     url: queryURL, 
-//     method:"GET"
-//   }).then(function(response){
-//     console.log(response)
-//     //Create a div to hold the anime
-//     var animeDiv=$("<div class='anime'>");
-//     //store the rating data
-//     for(var i = 0; i < response.data.length; i++){
-//       var rating = response.data[i].attributes.averageRating
-//       console.log(rating)
-//       var slug= response.data[i].attributes.slug
-//       console.log(slug)
-//       var canonicalTitle=response.data[i].attributes.canonicalTitle
-//       console.log(canonicalTitle)
-//       var description= response.data[i].attributes.synopsis
-//       //var image=response.data[i].attributes.coverImage.original
-//     }
-
-//     //create an element to hold the rating
-//     var pOne=$('<p>').text('Rating:'+rating)
-//     //displaying ther rating
-//     animeDiv.append(pOne);
-//     //creat element to hold the slug
-//     var pTwo=$('<p>').text('Slug'+slug)
-//     //display slug
-//     animeDiv.append(pTwo);
-//     // element to hold Title
-//     var pThree=$('<p>').text('CanonicalTitle'+canonicalTitle)
-//     //display the title
-//     animeDiv.append(pThree)
-//     // element to hold the plot 
-//     var pFour=$('<p>').text('Description'+description)
-//     // display the plot
-//     animeDiv.append(pFour)
-//     //relement to hold the image
-//     //var iOne=$('img').attr('src',image)
-//     // append to display
-//     //animeDiv.append(iOne)
-//     //putting entire object into animeDiv Element
-//     $('#anime-view').prepend(animeDiv);
-//     console.log(rating)
-//   });}
+function sAnime(searchAnime){
+  
+  
+$.ajax({
+    url:"https://kitsu.io/api/edge/anime?include=streamingLinks&filter[slug]="+searchAnime,
+    method:"GET"
+  }).then(function(response){
+    var animeDiv=$("<div class='anime'>");
+  //for the streaming service they are on two different arrays 
+    for(var i=0;i<response.included.length;i++) {
+    console.log(response.included[i].attributes.url)
+    var streamingService=(response.included[i].attributes.url)
+    console.log(streamingService)
+//loop for the attributes
+    }for(var i=0;i<response.data.length;i++){
+      console.log(response.data[i].attributes.ageRating)
+      //var for the rating
+      var Arating=(response.data[i].attributes.ageRating)
+      //var fot the title
+      var Atitle=(response.data[i].attributes.canonicalTitle)
+      //var fot the description
+      var Danime=(response.data[i].attributes.synopsis)
+      var YID=(response.data[i].attributes.youtubeVideoId)
 
 
-// //button click event
-// $('#add-anime').on('click',function(event){
-//   event.preventDefault();
-//   var anime=$('#anime-input').val().trim();
-//   displayAnimeInfo(anime)})
+    }
+    //create an element to hold Streaming serive
+    var sOne=$("<ul><br><li><br><li><br><li><ul>").text('streamingService:'+streamingService)
+    //displaying ther service
+    animeDiv.append(sOne);
+    //create element to hold the age rating
+    var pOne=$('<p>').text(''+Arating)
+    //display the age rating
+    animeDiv.append(pOne)
+    //create an element to hold the title 
+    var at=$('<h1>').text(''+Atitle)
+    //display title
+    animeDiv.append(at)
+    //create an element t o hold the synopsis
+    var as=$('<p>').text('Synopsis: '+Danime)
+    //display synopsis
+    animeDiv.append(as)
+    //creat an element to hold video
+    var vid=$("<iframe width='420' height='315'>").attr("src","https://www.youtube.com/embed/"+YID)
+    animeDiv.append(vid)
 
-// //run the function
-// displayAnimeInfo()
+
+    $('#anime-view').prepend(animeDiv)
+  })
+  
+  }
+  //create a button click event
+  $("#add-anime").on('click',function(event){
+  event.preventDefault();
+  var searchAnime=$("#anime-input").val();
+  sAnime(searchAnime)})
+
+    
+ 
+
+
+
+
 
 
 
@@ -107,36 +113,141 @@
 // sMovie('tt1234721')
 
 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 // calls back anime with the object that contains the streaming link 
-$.ajax({
-  url:"https://kitsu.io/api/edge/anime?include=streamingLinks&filter[slug]=sword-art-online",
-  method:"GET"
-}).then(function(response){
-  for(var i = 0; i < response.included.length; i++)
-  console.log(response.included[i].attributes.url)
-})
+// $.ajax({
+//   url:"https://kitsu.io/api/edge/anime?include=streamingLinks&filter[slug]=sword-art-online",
+//   method:"GET"
+// }).then(function(response){
+//   console.log(response)
+// })
 
 
-// create a function uses the slug to grab streaming link...
-function sAnime(searchAnime){
-  
-  
-$.ajax({
-    url:"https://kitsu.io/api/edge/anime?include=streamingLinks&filter[slug]="+searchAnime,
-    method:"GET"
-  }).then(function(response){
-    for(var i=0;i<response.included.length;i++) 
-    
-    console.log(response.included[i].attributes.url)})
-  }
-  
-    
-  sAnime('bleach')
 
-  $.ajax({
-    url:"https://kitsu.io/api/edge/anime?include=streamingLinks&filter[slug]=sword-art-online",
-    method:"GET"
-  }).then(function(response){
-    
-    console.log(response)
-  })
+
+
+  // function sAnimeAttributes(searchAnime){
+  
+  
+  //   $.ajax({
+  //       url:"https://kitsu.io/api/edge/anime?include=streamingLinks&filter[slug]="+searchAnime,
+  //       method:"GET"
+  //     }).then(function(response){
+  //       for(var i=0;i<response.data.length;i++) 
+  //       console.log(response.data[i].attributes.ageRating)
+        
+      
+  //     })
+  //     }
+
+  // sAnimeAttributes("bleach")
+
+
+
+  // //function to display anime Info 
+// function displayAnimeInfo(anime){
+//   var queryURL="https://kitsu.io/api/edge/anime?filter%5Btext%5D="+anime;
+
+//   //ajax call
+//   $.ajax({
+//     url: queryURL, 
+//     method:"GET"
+//   }).then(function(response){
+//     console.log(response)
+//     //Create a div to hold the anime
+//     var animeDiv=$("<div class='anime'>");
+//     //store the attribute data
+//     for(var i = 0; i < response.data.length; i++){
+//       var rating = response.data[i].attributes.averageRating
+//       console.log(rating)
+//       var slug= response.data[i].attributes.slug
+//       console.log(slug)
+//       var canonicalTitle=response.data[i].attributes.canonicalTitle
+//       console.log(canonicalTitle)
+//       var description= response.data[i].attributes.synopsis
+//       //var image=response.data[i].attributes.coverImage.original
+//     }
+
+//     //create an element to hold the rating
+//     var pOne=$('<p>').text('Rating:'+rating)
+//     //displaying ther rating
+//     animeDiv.append(pOne);
+//     //creat element to hold the slug
+//     var pTwo=$('<p>').text('Slug'+slug)
+//     //display slu
+//     animeDiv.append(pTwo);
+//     // element to hold Title
+//     var pThree=$('<p>').text('CanonicalTitle'+canonicalTitle)
+//     //display the title
+//     animeDiv.append(pThree)
+//     // element to hold the plot 
+//     var pFour=$('<p>').text('Description'+description)
+//     // display the plot
+//     animeDiv.append(pFour)
+//     //relement to hold the image
+//     //var iOne=$('img').attr('src',image)
+//     // append to display
+//     //animeDiv.append(iOne)
+//     //putting entire object into animeDiv Element
+//     $('#anime-view').prepend(animeDiv);
+//     console.log(rating)
+//   });}
+
+
+// //button click event
+// $('#add-anime').on('click',function(event){
+//   event.preventDefault();
+//   var anime=$('#anime-input').val().trim();
+//   displayAnimeInfo(anime)})
+// //run the function
+// displayAnimeInfo()
+
+
+
+
+
+// // create a function uses the slug to grab streaming link...
