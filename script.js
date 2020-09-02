0ed1977514cf3fc39fbdbc67a1bcba5b046b2c2f
LiliClift <65939150+LiliCecilia23@users.noreply.github.com>
2020-09-02 16:38:46 -0400

    trying to make show services div populate correctly

--- script.js
+++ script.js
@@ -67,11 +67,11 @@ function displayTitleResults(searchMovie){
         listItem.append(titleDesc)
         $("#resultsList").append(listItem)
         $("#showResultsDiv").removeClass("is-hidden")
+
         $(".movieSel").on("click", function(){
             titleSearch = $(this).attr("title-search")
             results = JSON.parse(localStorage.getItem(titleSearch))
             titleIndex = $(this).attr("title-index")
-            console.log(results[titleIndex].title)
             $("#showInfoDiv").removeClass("is-hidden");
             $("#titleSelect").text(results[titleIndex].title + " (" + results[titleIndex].year + ")");
             $("#picture").attr("src", results[titleIndex].picture);
@@ -80,8 +80,50 @@ function displayTitleResults(searchMovie){
             $("#selectDirector").text(results[titleIndex].director);
             $("#selectDesc").text(results[titleIndex].plot);
             $("#selectActors").text(results[titleIndex].actors);
-            for (var v = 0; v < results[titleIndex].streams.length; v++)
-                $("#yourServices").html("<li>❂ " + "<a href=" + results[titleIndex].streams[v] + ">Watch Here</a></li>")
+            for (var s = 0; s < results[titleIndex].streams.length; s++) {
+                var service;
+                var streamUrls = results[titleIndex].streams[s];
+                console.log(results[titleIndex].streams[s])
+                if (streamUrls.includes("netflix")){
+                    service = "Netflix";
+                } else if (streamUrls.includes("itunes.apple")){
+                    service = "iTunes";
+                } else if (streamUrls.includes("watch.amazon")){
+                    service = "Amazon Prime";
+                } else if (streamUrls.includes("hulu")){
+                    service = "Hulu";
+                } else if (streamUrls.includes("youtube")){
+                    service = "Youtube Premium";
+                } else if (streamUrls.includes("disney")){
+                    service = "Disney Plus";
+                } else if (streamUrls.includes("hbo")){
+                    service = "HBO";
+                } else if (streamUrls.includes("cbs")){
+                    service = "CBS";
+                } else if (streamUrls.includes("fox")){
+                    service = "Fox";
+                } else if (streamUrls.includes("nbc")){
+                    service = "NBC";
+                } else if (streamUrls.includes("nick")){
+                    service = "Nickelodeon";
+                } else if (streamUrls.includes("play.google")){
+                    service = "Google Play";
+                } else if (streamUrls.includes("dcuniverse")){
+                    service = "DC Universe";
+                } else if (streamUrls.includes(".discovery")){
+                    service = "Discovery Channel";
+                } else if (streamUrls.includes("tv.apple")){
+                    service = "Apple TV";
+                } else {
+                    service = "dunno";
+                }
+                
+                $("#yourServices").html("<li>❂ " + service + "<a href=" + results[titleIndex].streams[s] + "> Watch Here</a></li>");
+
+
+            
+            }
+
         })
     }
 }
