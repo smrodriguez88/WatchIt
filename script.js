8b2fbf469876e697d4d5c84cf802c32c60fe5fa1
LiliClift <65939150+LiliCecilia23@users.noreply.github.com>
2020-09-03 15:08:19 -0400

    removing rapid api key again

--- script.js
+++ script.js
@@ -18,7 +18,7 @@ function utellyMovie(searchMovie){
         "method": "GET",
         "headers": {
             "x-rapidapi-host": "utelly-tv-shows-and-movies-availability-v1.p.rapidapi.com",
-            "x-rapidapi-key": "5cd25c1681mshc17a6de27e4095fp17a9c9jsna97853c66886"
+            "x-rapidapi-key": ""
         }
     }
 $.ajax(settings).done(function (response) {
